b7c07dd2736708a3181042944db67085f8d34564
Michael Shonov <waveafterwave3w2@gmail.com>
2025-05-18 21:00:31 +0300

    fix

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react'
 import Profile from '../../components/Profile/Profile'
 
 export default function ProfilePage() {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
+
     return (
         <>
             <Profile />
